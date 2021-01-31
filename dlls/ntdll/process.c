96bade46d32db6d9945ea76395d8f7ea84c47eb3
Spaceman <rionlion100@gmail.com>
2021-01-30 20:32:36 -0500

    Update process.c

--- dlls/ntdll/process.c
+++ dlls/ntdll/process.c
@@ -18,10 +18,9 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
  */
-
+#include "config.h"
 #include <errno.h>
 #include <fcntl.h>
-#include "config.h"
 
 #include <stdarg.h>
 #include <stdio.h>
